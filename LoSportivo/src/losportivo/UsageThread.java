136c4e2d562841381d2d35a84dc030c1a48a7550
BernicchiDavide <bernicchi.davidemaria@franchettisalviani.net>
2026-03-12 17:21:15 +0100

    creazione classi

---
+++ LoSportivo/src/losportivo/UsageThread.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package losportivo;
+
+/**
+ *
+ * @author 39340
+ */
+public class UsageThread {
+    
+}
